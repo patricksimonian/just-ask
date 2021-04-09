9432e29f987312c614b5cc090cfd59a60eed1692
Patrick Simonian <patricksimonian@gmail.com>
2021-04-09 09:17:44 -0700

    introduce linting

--- api/tests/roles.test.js
+++ api/tests/roles.test.js
@@ -6,128 +6,183 @@ import orgMembership from "../fixtures/orgMembership";
 import orgTeamMembership from "../fixtures/orgTeamMembership";
 import roleMapping from "../fixtures/roleMapping";
 import { getConfig, getRoleMapping } from "../utils/config";
-import { doesUserHaveRole, getOrgRoleForUser, getTeamMembershipForOrg, resolveGithubTeam, resolveOrgRole } from "../utils/roles";
+import {
+  doesUserHaveRole,
+  getOrgRoleForUser,
+  getTeamMembershipForOrg,
+  resolveGithubTeam,
+  resolveOrgRole,
+} from "../utils/roles";
 
-jest.mock('../utils/config.js');
-const orgs = installations.filter(installation => installation.target_type === 'Organization');
+jest.mock("../utils/config.js");
+const orgs = installations.filter(
+  (installation) => installation.target_type === "Organization"
+);
 
-describe('Role utilities tests', () => {
+describe("Role utilities tests", () => {
   afterEach(() => {
     nock.cleanAll();
   });
 
-  describe('getOrgRoleForUser', () => {
-    it('throws if the team membership org is not a valid installation', async () => {
-      jest.unmock('../utils/init');
-      nock('https://api.github.com').get('/app/installations')
-      .reply(200, installations)
+  describe("getOrgRoleForUser", () => {
+    it("throws if the team membership org is not a valid installation", async () => {
+      jest.unmock("../utils/init");
+      nock("https://api.github.com")
+        .get("/app/installations")
+        .reply(200, installations);
 
-      getConfig.mockReturnValueOnce(({orgs: orgs.map(o => o.account.login)}));
-      await expect(getOrgRoleForUser('foo', 'bar')).rejects.toThrow('Unable to get org information for foo. The org bar is not a valid installation');
+      getConfig.mockReturnValueOnce({ orgs: orgs.map((o) => o.account.login) });
+      await expect(getOrgRoleForUser("foo", "bar")).rejects.toThrow(
+        "Unable to get org information for foo. The org bar is not a valid installation"
+      );
     });
 
-    it('returns role', async () => {
-      getConfig.mockReturnValueOnce(({orgs: orgs.map(o => o.account.login)}));
-      nock('https://api.github.com').get('/app/installations')
-      .reply(200, installations)
-      .post('/app/installations/15321559/access_tokens')
-      .reply(201, accessToken)
-      .get(`/orgs/${orgs[0].account.login}/memberships/${orgMembership.user.login}`)
-      .reply(200, orgMembership);
+    it("returns role", async () => {
+      getConfig.mockReturnValueOnce({ orgs: orgs.map((o) => o.account.login) });
+      nock("https://api.github.com")
+        .get("/app/installations")
+        .reply(200, installations)
+        .post("/app/installations/15321559/access_tokens")
+        .reply(201, accessToken)
+        .get(
+          `/orgs/${orgs[0].account.login}/memberships/${orgMembership.user.login}`
+        )
+        .reply(200, orgMembership);
 
-      const role = await getOrgRoleForUser(orgMembership.user.login, orgs[0].account.login);
+      const role = await getOrgRoleForUser(
+        orgMembership.user.login,
+        orgs[0].account.login
+      );
 
       expect(role).toBe(orgMembership.role);
     });
   });
-  
-  describe('getTeamMembershipForOrg', () => {
-    it('throws if the team membership org is not a valid installation', async () => {
-      nock('https://api.github.com').get('/app/installations')
-      .reply(200, installations)
 
-      getConfig.mockReturnValueOnce(({orgs: orgs.map(o => o.account.login)}));
-      await expect(getTeamMembershipForOrg('foo', 'bar', 'team')).rejects.toThrow('Unable to get org information for foo. The org bar is not a valid installation');
+  describe("getTeamMembershipForOrg", () => {
+    it("throws if the team membership org is not a valid installation", async () => {
+      nock("https://api.github.com")
+        .get("/app/installations")
+        .reply(200, installations);
+
+      getConfig.mockReturnValueOnce({ orgs: orgs.map((o) => o.account.login) });
+      await expect(
+        getTeamMembershipForOrg("foo", "bar", "team")
+      ).rejects.toThrow(
+        "Unable to get org information for foo. The org bar is not a valid installation"
+      );
     });
-    
-    it('returns user if is apart of team', async () => {
-      
-      getConfig.mockReturnValueOnce(({orgs: orgs.map(o => o.account.login)}));
-      nock('https://api.github.com').get('/app/installations')
-      .reply(200, installations)
-      .post('/app/installations/15321559/access_tokens')
-      .reply(201, accessToken)
-      .get(`/orgs/${orgs[0].account.login}/teams/team`)
-      .reply(200, orgTeamMembership);
 
-      const user = await getTeamMembershipForOrg(orgTeamMembership[0].login, orgs[0].account.login, 'team');
-      
+    it("returns user if is apart of team", async () => {
+      getConfig.mockReturnValueOnce({ orgs: orgs.map((o) => o.account.login) });
+      nock("https://api.github.com")
+        .get("/app/installations")
+        .reply(200, installations)
+        .post("/app/installations/15321559/access_tokens")
+        .reply(201, accessToken)
+        .get(`/orgs/${orgs[0].account.login}/teams/team`)
+        .reply(200, orgTeamMembership);
+
+      const user = await getTeamMembershipForOrg(
+        orgTeamMembership[0].login,
+        orgs[0].account.login,
+        "team"
+      );
+
       expect(user).toBeDefined();
     });
   });
 
-  describe('doesUserHaveRole', () => {
-    it('returns true if role is not approver and there are no role mappings', async () => {
+  describe("doesUserHaveRole", () => {
+    it("returns true if role is not approver and there are no role mappings", async () => {
       getRoleMapping.mockReturnValueOnce(roleMapping);
-      await expect(doesUserHaveRole(ROLES.REQUESTER, 'mattdamon')).resolves.toBe(true);
+      await expect(
+        doesUserHaveRole(ROLES.REQUESTER, "mattdamon")
+      ).resolves.toBe(true);
     });
-    
+
     it(`returns true for APPROVER when user ${orgTeamMembership[0].login} has admin role ${orgs[0].account.login}`, async () => {
       getRoleMapping.mockReturnValueOnce(roleMapping);
-      getConfig.mockReturnValueOnce(({orgs: orgs.map(o => o.account.login)}));
-      nock('https://api.github.com').get('/app/installations')
-      .reply(200, installations)
-      .post('/app/installations/15321559/access_tokens')
-      .reply(201, accessToken)
-      .get(`/orgs/${orgs[0].account.login}/memberships/${orgMembership.user.login}`)
-      .reply(200, orgMembership)
-      .get(`/orgs/${orgs[0].account.login}/teams/team`)
-      .reply(200, orgTeamMembership);
-      await expect(doesUserHaveRole(ROLES.APPROVER, orgTeamMembership[0].login)).resolves.toBe(true);
-    })
-  });
-
-  describe('resolveGithubTeam', () => {
-    it('returns false if org is not a valid installation', async () => {
-      nock('https://api.github.com').get('/app/installations')
-      .reply(200, installations)
-
-      getConfig.mockReturnValueOnce(({orgs: orgs.map(o => o.account.login)}));
-      await expect(resolveGithubTeam('foo', 'mattdamon', 'team')).resolves.toBe(false);
-    });
-
-    it('returns true if user is found', async () => {
-      getConfig.mockReturnValueOnce(({orgs: orgs.map(o => o.account.login)}));
-      nock('https://api.github.com').get('/app/installations')
-      .reply(200, installations)
-      .post('/app/installations/15321559/access_tokens')
-      .reply(201, accessToken)
-      .get(`/orgs/${orgs[0].account.login}/teams/team`)
-      .reply(200, orgTeamMembership);
-
-      await expect(resolveGithubTeam(orgTeamMembership[0].login, orgs[0].account.login, 'team')).resolves.toBe(true);
+      getConfig.mockReturnValueOnce({ orgs: orgs.map((o) => o.account.login) });
+      nock("https://api.github.com")
+        .get("/app/installations")
+        .reply(200, installations)
+        .post("/app/installations/15321559/access_tokens")
+        .reply(201, accessToken)
+        .get(
+          `/orgs/${orgs[0].account.login}/memberships/${orgMembership.user.login}`
+        )
+        .reply(200, orgMembership)
+        .get(`/orgs/${orgs[0].account.login}/teams/team`)
+        .reply(200, orgTeamMembership);
+      await expect(
+        doesUserHaveRole(ROLES.APPROVER, orgTeamMembership[0].login)
+      ).resolves.toBe(true);
     });
   });
 
-  describe('resolveOrgRole', () => {
-    it('returns false if org is not a valid installation', async () => {
-      nock('https://api.github.com').get('/app/installations')
-      .reply(200, installations)
+  describe("resolveGithubTeam", () => {
+    it("returns false if org is not a valid installation", async () => {
+      nock("https://api.github.com")
+        .get("/app/installations")
+        .reply(200, installations);
 
-      getConfig.mockReturnValueOnce(({orgs: orgs.map(o => o.account.login)}));
-      await expect(resolveOrgRole('foo', 'mattdamon', 'admin')).resolves.toBe(false);
+      getConfig.mockReturnValueOnce({ orgs: orgs.map((o) => o.account.login) });
+      await expect(resolveGithubTeam("foo", "mattdamon", "team")).resolves.toBe(
+        false
+      );
     });
 
-    it('returns true if user is found', async () => {
-      getConfig.mockReturnValueOnce(({orgs: orgs.map(o => o.account.login)}));
-      nock('https://api.github.com').get('/app/installations')
-      .reply(200, installations)
-      .post('/app/installations/15321559/access_tokens')
-      .reply(201, accessToken)
-      .get(`/orgs/${orgs[0].account.login}/memberships/${orgMembership.user.login}`)
-      .reply(200, orgMembership);
+    it("returns true if user is found", async () => {
+      getConfig.mockReturnValueOnce({ orgs: orgs.map((o) => o.account.login) });
+      nock("https://api.github.com")
+        .get("/app/installations")
+        .reply(200, installations)
+        .post("/app/installations/15321559/access_tokens")
+        .reply(201, accessToken)
+        .get(`/orgs/${orgs[0].account.login}/teams/team`)
+        .reply(200, orgTeamMembership);
 
-      await expect(resolveOrgRole(orgMembership.user.login, orgs[0].account.login, orgMembership.role)).resolves.toBe(true);
+      await expect(
+        resolveGithubTeam(
+          orgTeamMembership[0].login,
+          orgs[0].account.login,
+          "team"
+        )
+      ).resolves.toBe(true);
+    });
+  });
+
+  describe("resolveOrgRole", () => {
+    it("returns false if org is not a valid installation", async () => {
+      nock("https://api.github.com")
+        .get("/app/installations")
+        .reply(200, installations);
+
+      getConfig.mockReturnValueOnce({ orgs: orgs.map((o) => o.account.login) });
+      await expect(resolveOrgRole("foo", "mattdamon", "admin")).resolves.toBe(
+        false
+      );
+    });
+
+    it("returns true if user is found", async () => {
+      getConfig.mockReturnValueOnce({ orgs: orgs.map((o) => o.account.login) });
+      nock("https://api.github.com")
+        .get("/app/installations")
+        .reply(200, installations)
+        .post("/app/installations/15321559/access_tokens")
+        .reply(201, accessToken)
+        .get(
+          `/orgs/${orgs[0].account.login}/memberships/${orgMembership.user.login}`
+        )
+        .reply(200, orgMembership);
+
+      await expect(
+        resolveOrgRole(
+          orgMembership.user.login,
+          orgs[0].account.login,
+          orgMembership.role
+        )
+      ).resolves.toBe(true);
     });
   });
 });
--- api/utils/roles.js
+++ api/utils/roles.js
@@ -1,8 +1,7 @@
 import log from "log";
 import { ROLES, ROLE_MAPPING_KINDS } from "../constants";
 import { getRoleMapping } from "./config";
-import { getAuthenticatedApps } from "./init"
-
+import { getAuthenticatedApps } from "./init";
 
 /**
  * checks a user against an org role and returns boolean if they meet the spec
@@ -16,13 +15,11 @@ export const resolveOrgRole = async (username, org, role) => {
     const orgMembershipRole = await getOrgRoleForUser(username, org);
 
     return role === orgMembershipRole;
-  } catch(e) {
-
+  } catch (e) {
     log.error(e.message);
     return false;
-  }   
-}
-
+  }
+};
 
 /**
  * checks if user is apart of specific org team
@@ -36,56 +33,53 @@ export const resolveGithubTeam = async (username, org, team) => {
     const user = await getTeamMembershipForOrg(username, org, team);
 
     return !!user;
-  } catch(e) {
-
+  } catch (e) {
     log.error(e);
     return false;
   }
-}
-
+};
 
 /**
  * checks if user has a app role
- * @param {String} role the app role as defined in constants.ROLE_MAPPING_KINDS 
+ * @param {String} role the app role as defined in constants.ROLE_MAPPING_KINDS
  * @param {String} username the github user
  * @returns {Promise<Boolean>}
  */
 export const doesUserHaveRole = async (role, username) => {
   const mappings = getRoleMapping();
-  
+
   // special case where you can grant access to role if configured to not look for anything special
-  if(role !== ROLES.APPROVER && mappings[role][0] === null) return true;
+  if (role !== ROLES.APPROVER && mappings[role][0] === null) return true;
 
-  const checks = await Promise.all(mappings[role].map(async mapper => {
+  const checks = await Promise.all(
+    mappings[role].map(async (mapper) => {
+      switch (mapper.kind) {
+        case ROLE_MAPPING_KINDS.OrgRole:
+          return resolveOrgRole(username, mapper.organization, mapper.role);
 
-    switch(mapper.kind) {
+        case ROLE_MAPPING_KINDS.GithubTeam:
+          return resolveGithubTeam(username, mapper.organization, mapper.team);
 
-      case ROLE_MAPPING_KINDS.OrgRole:
-        return resolveOrgRole(username, mapper.organization, mapper.role);
-
-      case ROLE_MAPPING_KINDS.GithubTeam:
-        return resolveGithubTeam(username, mapper.organization, mapper.team);
-
-      default:
-        return false;
-    }
-
-  }));
+        default:
+          return false;
+      }
+    })
+  );
 
   return checks.includes(true);
-}
+};
 
 /**
  * attempts to get user from an org team
- * @param {String} username 
- * @param {String} org 
- * @param {String} team 
+ * @param {String} username
+ * @param {String} org
+ * @param {String} team
  * @returns {Promise<GithubUser>}
  */
 export const getTeamMembershipForOrg = async (username, org, team) => {
   const installations = await getAuthenticatedApps();
 
-  if(!installations.apps[org]) {
+  if (!installations.apps[org]) {
     const message = `Unable to get org information for ${username}. The org ${org} is not a valid installation`;
     log.warn(message);
     throw new Error(message);
@@ -93,36 +87,41 @@ export const getTeamMembershipForOrg = async (username, org, team) => {
 
   const installation = installations.apps[org];
 
-  const response = await installation.authenticatedRequest('GET /orgs/{org}/teams/{team}', {
-    org,
-    team
-  });
+  const response = await installation.authenticatedRequest(
+    "GET /orgs/{org}/teams/{team}",
+    {
+      org,
+      team,
+    }
+  );
 
-  return response.data.find(member => member.login === username);
-}
-
+  return response.data.find((member) => member.login === username);
+};
 
 /**
  * get org role for user
- * @param {String} username 
- * @param {String} org 
+ * @param {String} username
+ * @param {String} org
  * @returns {Promise<String>}
  */
 export const getOrgRoleForUser = async (username, org) => {
   const installations = await getAuthenticatedApps();
   const installation = installations.apps[org];
 
-  if(!installation) {
+  if (!installation) {
     const message = `Unable to get org information for ${username}. The org ${org} is not a valid installation`;
     log.warn(message);
     throw new Error(message);
   }
 
   // make request for org membership
-  const response = await installation.authenticatedRequest('GET /orgs/{org}/memberships/{username}', {
-    org,
-    username
-  });
+  const response = await installation.authenticatedRequest(
+    "GET /orgs/{org}/memberships/{username}",
+    {
+      org,
+      username,
+    }
+  );
 
   return response.data.role;
-}
+};

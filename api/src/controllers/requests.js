d169544d62a6098727072a7b067ffbf28e7388df
Alex Carmichael <carmichael.alexander@gmail.com>
2021-10-27 11:52:12 -0700

    Removed method  that was moved to other class

--- api/src/controllers/requests.js
+++ api/src/controllers/requests.js
@@ -8,7 +8,6 @@ import { createAudit } from '../utils/audit'
 import {
   getUserByName,
   inviteUserToOrgs,
-  getRequestStatuses,
 } from '../utils/github'
 
 /**
@@ -420,38 +419,6 @@ export const createInvitationRequest = async (req, res) => {
     log.error(e)
     res.status(500).send({
       message: 'Unable to create invitation',
-    })
-  }
-}
-
-export const getUserPendingInvitations = async (req, res) => {
-  log.info('getUserPendingInvitations')
-
-  await createAudit({
-    apiVersion: 'v1',
-    action: AUDIT_ACTIONS.api.requests.getUserPendingInvitations,
-    data: JSON.stringify({
-      message: `user attempting to view pending requests`,
-      user: req.auth.user,
-      payload: req.body,
-      type: 'info',
-    }),
-  })
-
-  try {
-    const requests = await InvitationRequest.find({
-      requester: req.auth.user,
-    }).exec()
-    //  we have the requests made by the user (on other peoples'  behalf), now use github api to see the real status of those requests
-    const requestStatuses = await getRequestStatuses(requests)
-    log.info(requestStatuses)
-    res.status(200).json(requestStatuses)
-
-  } catch (e) {
-    log.warn(`user ${req.auth.user} request failed`)
-    log.debug(e)
-    res.status(500).send({
-      message: "Unable to  fetch user's pending requests",
     })
   }
 }
